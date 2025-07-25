0bf64caf2ce27e22a58d458a685adfdf3c11b3d4
nelricardoj <nelricardoj@gmail.com>
2025-07-25 10:47:20 +0200

    Update index.js
    Remove port

--- heroku/index.js
+++ heroku/index.js
@@ -70,7 +70,7 @@ app.post('/threads', function(req, res) {
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => { 
-  console.log('Server is running on port ${port}');
+  console.log('Server is running on port');
 });
 
 //app.listen();
