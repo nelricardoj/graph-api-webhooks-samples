6629c4049ad31344af0b873e8bb0f2f77d04e2ce
nelricardoj <nelricardoj@gmail.com>
2025-07-25 10:40:23 +0200

    Update index.js
    rollback

--- heroku/index.js
+++ heroku/index.js
@@ -67,7 +67,4 @@ app.post('/threads', function(req, res) {
   res.sendStatus(200);
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log('Server is running on port ${port}');
-});
+app.listen();
