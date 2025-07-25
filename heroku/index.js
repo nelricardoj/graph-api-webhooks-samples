914fa6f4d06e4dd37840f04d5699a85b4e74a20f
nelricardoj <nelricardoj@gmail.com>
2025-07-25 10:21:40 +0200

    Update index.js
    Add additional info for logging

--- heroku/index.js
+++ heroku/index.js
@@ -67,4 +67,7 @@ app.post('/threads', function(req, res) {
   res.sendStatus(200);
 });
 
-app.listen();
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log('Server is running on port ${port}');
+});
